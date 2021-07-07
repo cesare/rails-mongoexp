2c06c65b19a1f64e0cbd0f8abbcc34842030eea4
SAWADA Tadashi <cesare@mayverse.jp>
2021-07-07 21:05:08 +0900

    Enable to run specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,10 +4,22 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'database_cleaner/mongoid'
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :deletion
+    DatabaseCleaner[:mongoid].clean_with(:deletion)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
