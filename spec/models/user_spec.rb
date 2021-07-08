26ac453695e275a7586a9deb2fd47ba85c69f94d
SAWADA Tadashi <cesare@mayverse.jp>
2021-07-08 15:48:08 +0900

    Add dummy spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,14 @@
 RSpec.describe User do
+  describe "#save" do
+    specify do
+      user = User.with_session do |s|
+        s.start_transaction
+        user = User.create!(name: "test")
+        s.commit_transaction
+        user
+      end
+
+      expect(user).to be_persisted
+    end
+  end
 end
